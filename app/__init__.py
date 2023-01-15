99c0915f87eefa97081ca51220c3ccd4d87b0dcf
Roshan <kan075bct048@kec.edu.np>
2023-01-15 17:13:11 +0545

    config file import

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask,render_template
-
+from config import username
+from config import password
+from config import host
+from config import database
+from config import port
 
 
 app = Flask(__name__)
 
+
 from app import views 
+from app import hello
 
 
