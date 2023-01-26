f0b7fc4362a5914f77783710b093e3f8ab76db42
Roshan <kan075bct048@kec.edu.np>
2023-01-26 10:45:51 +0545

    To deal with table changes

--- app/update_db_info.py
+++ app/update_db_info.py
@@ -1,15 +1,9 @@
 import psycopg2
-# from app import username
-# from app import password
-# from app import host
-# from app import database
-# from app import port
-
-username = 'postgres'
-password = '599433'
-host = 'localhost'
-database = 'scraper'
-port = '5432'
+from app import username
+from app import password
+from app import host
+from app import database
+from app import port
 def update_df_info(path):   
  
     try:
@@ -22,8 +16,6 @@ def update_df_info(path):
         cursor = connection.cursor()
         update_query = f"UPDATE scraping_requests SET scraping_status = True  WHERE path_to_file = '{path}'"   
         cursor.execute(update_query)   
-
-        print('Data has been successfully uploadedd')
     except (Exception, psycopg2.Error) as error:
        print(f'Data couldnt be uploaded because of {error}')
         # print("Failed to insert record into person table", error)
@@ -33,4 +25,3 @@ def update_df_info(path):
             cursor.close()
             connection.close()
 
-update_df_info('./TextFiles/Google.txt')
