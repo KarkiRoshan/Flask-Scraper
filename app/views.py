db4b5fce1f1afba0439eeda9a85e9db3a3110206
Roshan <kan075bct048@kec.edu.np>
2023-01-26 10:46:15 +0545

    Integrating changes

--- app/views.py
+++ app/views.py
@@ -1,16 +1,18 @@
 from app import app 
-from flask import request,render_template,send_from_directory
+from flask import request,render_template,send_from_directory,redirect
 from .scrape import scraper
 from .formatter import df_formatter
 import pandas as pd 
+from datetime import datetime
 import os 
 from .db_uploader import csv_to_db
-from .get_all_tables import get_all_tables
+from .get_all_keys import get_all_keys
 from .files_info_db import file_info_update
 from .get_file_path import get_file_path
 from .update_db_info import update_df_info
 from .get_data_from_db import get_data_from_db
-
+from .zip import zip
+from .horizontal_data import horizontal_data
 @app.route("/", methods=["GET"])
 def index():
      return render_template("index.html")
@@ -20,73 +22,84 @@ def get_txt_file():
     if request.method=='POST':
         try:    
             f = request.files['textfile']
-            f_path = './TextFiles'
-            f_name = f.filename
-            file_full_path = f'{f_path}/{f_name}'
-            f.save(file_full_path)
-            file_info_update(file_full_path)
-            return render_template("index.html",success='File uploaded sucessfully')
+
+            actual_f_name = f.filename
+            if (actual_f_name.split('.')[-1]=='txt'):
+                f_path = './TextFiles'
+                now = datetime.now()
+                timestamp_filename = now.strftime("%Y-%m-%d-%H-%M-%S")
+                unique_fname = actual_f_name.split('.')[0] + ''.join(timestamp_filename.split('-'))
+
+                file_full_path = f'{f_path}/{timestamp_filename}.txt'
+                f.save(file_full_path)
+                status =file_info_update(file_full_path,actual_f_name,now,unique_fname)
+                # return status
+                return render_template("index.html",success='File uploaded sucessfully')
+            else:
+                return render_template("index.html",success='Please Submit a textFile')
         except:
             return render_template("index.html",success='File couldnt be uploaded')
 
 @app.route("/scrape", methods=["GET"])
-def scrape_data():
-    try:
-        row = get_file_path()
-        full_path = row[0][0]
-        scraping_status = row[0][1]
-        if not scraping_status:
-            search_items = open(full_path,'r').read().split('\n')
-            count = 0 
-            for search_term in search_items:
-                scraper(search_term,count)
+def show_unscraped():
+
+        file_array = get_file_path()
+        for files in file_array:
+            files[0] = files[0].split('/')[-1]
+        return render_template('scrape.html',files=file_array)
+ 
+
+@app.route("/scrape/<filepath>", methods=["GET"])
+def scrape_data(filepath):
+        oldfilename = request.args.get('oldfilename')
+        oldfilename=oldfilename.split('.')[0]
+        time_stamp = filepath.split('-')
+        unique_table_name = oldfilename + ''.join(time_stamp)
+        search_items = open(f"./TextFiles/{filepath}",'r').read().split('\n')
+        count=0
+        for search_term in search_items:
+                scraper(search_term,count,unique_table_name.split('.')[0])
                 count+=1
-            update_df_info(full_path)
-            csv_dir = './CSV'
-            df = pd.read_csv(f'{csv_dir}/data_file.csv')
-            df['Desc'] = df['Desc'].str.replace(".*—","",regex=True)
-            df.to_csv(f'{csv_dir}/data_file.csv',index=False)
-            final_df =df_formatter(df)
-            final_df.to_csv(f'{csv_dir}/data_file.csv',index=False)
-            return render_template('index.html',data='DONE')
-        else:
-            return render_template('index.html',data='The data has already been scraped')
-    except:
-        return render_template('index.html',data='There is no data file to be scraped')
+        update_df_info(f"./TextFiles/{filepath}")
+        csv_dir = './CSV'
+        df = pd.read_csv(f'{csv_dir}/data_file.csv')
+        df['Desc'] = df['Desc'].str.replace(".*—","",regex=True)
+        df.to_csv(f'{csv_dir}/data_file.csv',index=False)
+        final_df =df_formatter(df)
+        final_df.to_csv(f'{csv_dir}/data_file.csv',index=False)
+        return redirect(f'/uploadtodb?arg={unique_table_name}')
+
+  
 
 
-# @app.route('/format',methods=['GET'])
-# def format_data():
-#     csv_dir = './CSV'
-#     df = pd.read_csv(f'{csv_dir}/data_file.csv')
-#     df['Desc'] = df['Desc'].str.replace(".*—","",regex=True)
-#     df.to_csv(f'{csv_dir}/data_file.csv',index=False)
-#     final_df =df_formatter(df)
-#     final_df.to_csv(f'{csv_dir}/data_file.csv',index=False)
-#     return('Data Has been formatted')    
-
-
-@app.route('/uploadtodb',methods=['POST'])
+@app.route('/uploadtodb',methods=['GET'])
 def upload_to_db():
-    # return
-     if request.method == "POST":
-        table_name = request.form['input_text']
-        table_list = get_all_tables()
-        if table_name in table_list:
-            return render_template('index.html',table_name_conflict=f'Please enter another tablename, {table_name} already exists ')
-        else:
-            status= csv_to_db(table_name)
-            return render_template('index.html',table_name_conflict=status)
+        arg = request.args.get('arg')
+        table_name = arg.split('.')[0]
+        status= csv_to_db(table_name)
+        return render_template('scrape.html',table_name_conflict=status)
 
 
 @app.route('/download',methods=['GET'])
-def get_table():
-    
-        table_list = get_all_tables()
-        return render_template('index.html',table_list=table_list)
+def get_table(): 
+        table_list = get_all_keys()
+        return render_template('tables.html',table_list=table_list)
 
 
 @app.route('/download/<filename>',methods=['GET'])
 def download_table(filename):
-        get_data_from_db(filename)
-        return send_from_directory('../CSV','downloaded.csv')
+        # return(filename)
+        status = get_data_from_db(filename)
+        horizontal_data(filename)
+        screenshots = os.listdir(f'./Screenshot/{filename}')
+        source_files = ['./CSV/downloaded.csv','./CSV/horizontal.csv']
+        for screenshot in screenshots:
+                if screenshot.split('.')[-1] != 'png':
+                        inner_dir = os.listdir(f'./Screenshot/{filename}/{screenshot}')
+                        for inner_file in inner_dir:
+                                source_files.append(f'./Screenshot/{filename}/{screenshot}/{inner_file}')
+                else:
+                        source_files.append(f'./Screenshot/{filename}/{screenshot}')
+        zip(source_files,'./ZipFile/zip.zip')
+        # return(status)
+        return send_from_directory('../ZipFile','zip.zip')
