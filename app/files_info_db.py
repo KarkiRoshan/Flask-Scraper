d6111132e542a1a20edecf4a0a798020a8e42cd0
Roshan <kan075bct048@kec.edu.np>
2023-02-07 09:34:07 +0545

    Added new column to db

--- app/files_info_db.py
+++ app/files_info_db.py
@@ -6,7 +6,7 @@ from app import database
 from app import port
 
 
-def file_info_update(file_path,actual_file_name,now,unique_fname):   
+def file_info_update(file_path,actual_file_name,now,unique_fname,platform):   
  
     try:
         connection = psycopg2.connect(user=username,
@@ -22,11 +22,12 @@ def file_info_update(file_path,actual_file_name,now,unique_fname):
                                 time varchar(50),
                                 path_to_file varchar(50),
                                 original_file_name varchar(50),
-                                scraping_status BOOLEAN)'''
+                                scraping_status BOOLEAN,
+                                platform varchar(10))'''
         cursor.execute(create_table)
-        insert_script='''INSERT INTO scraping_requests (uniquefilename,time,path_to_file,original_file_name,scraping_status)
-                        VALUES(%s,%s,%s,%s,%s)'''
-        insert_values = (unique_fname,now,file_path,actual_file_name,False)      
+        insert_script='''INSERT INTO scraping_requests (uniquefilename,time,path_to_file,original_file_name,scraping_status,platform)
+                        VALUES(%s,%s,%s,%s,%s,%s)'''
+        insert_values = (unique_fname,now,file_path,actual_file_name,False,platform)      
         cursor.execute(insert_script,insert_values)     
         status='Data has been successfully uploadedd'
     except (Exception, psycopg2.Error) as error:
